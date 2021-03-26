4a37cc63a893dc27e4dbed902fef15e4b2645720
karthikeyanvj <80826716+karthikeyanvj@users.noreply.github.com>
2021-03-26 15:54:57 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci():
+    a=0
+    b=1
+    for i in range(n):
+        print(a)
+        c=a
+        a=b
+        b=c+b
+n=int(input("Enter how many numbers you want in this series"))
+fibonacci()
+
---
+++ palindrome.py
@@ -0,0 +1,22 @@
+#Integer method
+"""s=input("Enter a number: ")
+p=(s[::-1])
+if p==s:    
+    print("The number is a palindrome")
+else:
+    print("Not a palnidrome")"""
+    
+#string method
+
+a=input("Enter a name: ")
+b=a
+c=(b[::-1])
+print(c)
+type(c)
+print(type(c))
+"""if b==c:
+    print(a,"is a palindrome")
+else:
+    print(a,"is not a palindrome")"""
+    
+      
