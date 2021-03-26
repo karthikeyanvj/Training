2f31b8e59e9ef2e4f424101bbf905dabfa7c4257
karthikeyanvj <80826716+karthikeyanvj@users.noreply.github.com>
2021-03-26 16:00:11 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,22 +1,20 @@
 #Integer method
-"""s=input("Enter a number: ")
+
+s=input("Enter a number: ")
 p=(s[::-1])
 if p==s:    
     print("The number is a palindrome")
 else:
     print("Not a palnidrome")"""
-    
+--------------------------------------------------------    
 #string method
 
 a=input("Enter a name: ")
 b=a
 c=(b[::-1])
-print(c)
-type(c)
-print(type(c))
-"""if b==c:
+if b==c:
     print(a,"is a palindrome")
 else:
-    print(a,"is not a palindrome")"""
+    print(a,"is not a palindrome")
     
       
